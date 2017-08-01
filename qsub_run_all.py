7f2e5c6f55f35f78f7ba12c9d9456b93f71c1ebb
Will Cureton <wcureton@vols.utk.edu>
2017-08-01 15:25:57 -0400

    Added comments

--- qsub_run_all.py
+++ qsub_run_all.py
@@ -1,17 +1,17 @@
 import os
 import time
 
-run_folders = 0
-for item in os.listdir(os.curdir):
+run_folders = 0 # Initialize counter
+for item in os.listdir(os.curdir): # Obtains list of run directories
 	if item.startswith('run') == True and os.path.isdir(item) == True:
 		run_folders += 1
 
-run_n=1
+run_n=1 # Initialize counter
 while run_n <= run_folders:
 	run_dir = os.path.join(os.curdir, 'run_{}'.format(run_n)) # setting path for run folder
-	for file in [f for f in os.listdir(run_dir) if f.endswith('.pbs')]:
-		os.chdir(run_dir)
-		os.system('qsub {}'.format(file))
-		os.chdir('..')
+	for file in [f for f in os.listdir(run_dir) if f.endswith('.pbs')]: # lists all files in run directory and finds submission script
+		os.chdir(run_dir) # changes to the run directory
+		os.system('qsub {}'.format(file)) # runs the job submission script within the run directory
+		os.chdir('..') # changes back to the master directory
 	run_n += 1
-	time.sleep(2)
+	time.sleep(2) # two second pause
